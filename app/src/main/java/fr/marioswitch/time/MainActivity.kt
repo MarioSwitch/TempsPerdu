41e91c03048d8d4e4a57049a1deac218ac9c01ae
MarioSwitch <fcuisine62@gmail.com>
2022-11-10 23:10:13 +0100

    2.1.1

--- app/src/main/java/fr/marioswitch/time/MainActivity.kt
+++ app/src/main/java/fr/marioswitch/time/MainActivity.kt
@@ -45,19 +45,21 @@ class MainActivity : AppCompatActivity() {
         }
         setContentView(R.layout.activity_main)
         val save = getSharedPreferences("fr.marioswitch.time",Context.MODE_PRIVATE)
-        var totalSeconds = save.getInt("total", 0)
+        var totalSeconds = save.getLong("total", 0)
 
         val mainHandler = Handler(Looper.getMainLooper())
         mainHandler.post(object : Runnable {
             override fun run() {
                 if(lifecycle.currentState.isAtLeast(Lifecycle.State.STARTED)){
                     totalSeconds++
-                    PlayGames.getLeaderboardsClient(this@MainActivity).submitScore(getString(R.string.leaderboard_id), (totalSeconds.toLong())*1000)
-                    save.edit().putInt("total",totalSeconds).apply()
-                    val seconds:Int = totalSeconds.rem(60)
-                    val minutes:Int = (totalSeconds/60).rem(60)
-                    val hours:Int = (totalSeconds/3600).rem(24)
-                    val days:Int = totalSeconds/86400
+                    if(totalSeconds.rem(60).toInt()==0){
+                        PlayGames.getLeaderboardsClient(this@MainActivity).submitScore(getString(R.string.leaderboard_id), totalSeconds*1000)
+                    }
+                    save.edit().putLong("total",totalSeconds).apply()
+                    val seconds:Int = totalSeconds.rem(60).toInt()
+                    val minutes:Int = (totalSeconds/60).rem(60).toInt()
+                    val hours:Int = (totalSeconds/3600).rem(24).toInt()
+                    val days:Int = (totalSeconds/86400).toInt()
                     when(totalSeconds){
                         in 0..59 -> time.text = buildString { append(seconds); append(getString(R.string.second)) }
                         in 60..3599 -> time.text = buildString { append(minutes); append(getString(R.string.minute)); append("%02d".format(seconds)); append(getString(R.string.second)) }
@@ -70,8 +72,10 @@ class MainActivity : AppCompatActivity() {
                         time_seconds.text = String.format(getString(R.string.time_seconds),totalSeconds)
                     }
                     val goals = arrayOf(0, 60, 300, 600, 1800, 3600, 7200, 14400, 25200, 43200, 86400, 259200, 604800, 1209600, 2592000)
-                    fun getLevel(seconds:Int): Int {
-                        for(i in 0..goals.size){
+                    val nextLevels = arrayOf(R.string.nextlevel0,R.string.nextlevel1,R.string.nextlevel2,R.string.nextlevel3,R.string.nextlevel4,R.string.nextlevel5,R.string.nextlevel6,R.string.nextlevel7,R.string.nextlevel8,R.string.nextlevel9,R.string.nextlevel10,R.string.nextlevel11,R.string.nextlevel12,R.string.nextlevel13,R.string.nextlevel14)
+                    val achievementsIDs = arrayOf(0,R.string.achievement01_id,R.string.achievement02_id,R.string.achievement03_id,R.string.achievement04_id,R.string.achievement05_id,R.string.achievement06_id,R.string.achievement07_id,R.string.achievement08_id,R.string.achievement09_id,R.string.achievement10_id,R.string.achievement11_id,R.string.achievement12_id,R.string.achievement13_id,R.string.achievement14_id)
+                    fun getLevel(seconds:Long): Int {
+                        for(i in goals.indices){
                             if(seconds<goals[i]){
                                 return i-1
                             }
@@ -98,22 +102,11 @@ class MainActivity : AppCompatActivity() {
                         14 -> diamond_icon.visibility = View.VISIBLE
                         else -> diamond_icon.visibility = View.INVISIBLE
                     }
-                    when(getLevel(totalSeconds)){
-                        0 -> nextlevel.text = getString(R.string.nextlevel0)
-                        1 -> nextlevel.text = getString(R.string.nextlevel1)
-                        2 -> nextlevel.text = getString(R.string.nextlevel2)
-                        3 -> nextlevel.text = getString(R.string.nextlevel3)
-                        4 -> nextlevel.text = getString(R.string.nextlevel4)
-                        5 -> nextlevel.text = getString(R.string.nextlevel5)
-                        6 -> nextlevel.text = getString(R.string.nextlevel6)
-                        7 -> nextlevel.text = getString(R.string.nextlevel7)
-                        8 -> nextlevel.text = getString(R.string.nextlevel8)
-                        9 -> nextlevel.text = getString(R.string.nextlevel9)
-                        10 -> nextlevel.text = getString(R.string.nextlevel10)
-                        11 -> nextlevel.text = getString(R.string.nextlevel11)
-                        12 -> nextlevel.text = getString(R.string.nextlevel12)
-                        13 -> nextlevel.text = getString(R.string.nextlevel13)
-                        14 -> nextlevel.text = getString(R.string.nextlevel14)
+                    for(i in 0..14){
+                        if(getLevel(totalSeconds)==i){
+                            nextlevel.text = getString(nextLevels[i])
+                            break
+                        }
                     }
                     when(getLevel(totalSeconds)){
                         in 5..9 -> completion.text = buildString { append(totalSeconds*100/goals[getLevel(totalSeconds)+1]); append(" %") }
@@ -123,24 +116,14 @@ class MainActivity : AppCompatActivity() {
                     when(getLevel(totalSeconds)){
                         in 11..14 -> level_title.text = ""
                     }
-                    if(totalSeconds>=goals[1]) PlayGames.getAchievementsClient(this@MainActivity).unlock(getString(R.string.achievement01_id))
-                    if(totalSeconds>=goals[2]) PlayGames.getAchievementsClient(this@MainActivity).unlock(getString(R.string.achievement02_id))
-                    if(totalSeconds>=goals[3]) PlayGames.getAchievementsClient(this@MainActivity).unlock(getString(R.string.achievement03_id))
-                    if(totalSeconds>=goals[4]) PlayGames.getAchievementsClient(this@MainActivity).unlock(getString(R.string.achievement04_id))
-                    if(totalSeconds>=goals[5]) PlayGames.getAchievementsClient(this@MainActivity).unlock(getString(R.string.achievement05_id))
-                    if(totalSeconds>=goals[6]) PlayGames.getAchievementsClient(this@MainActivity).unlock(getString(R.string.achievement06_id))
-                    if(totalSeconds>=goals[7]) PlayGames.getAchievementsClient(this@MainActivity).unlock(getString(R.string.achievement07_id))
-                    if(totalSeconds>=goals[8]) PlayGames.getAchievementsClient(this@MainActivity).unlock(getString(R.string.achievement08_id))
-                    if(totalSeconds>=goals[9]) PlayGames.getAchievementsClient(this@MainActivity).unlock(getString(R.string.achievement09_id))
-                    if(totalSeconds>=goals[10]) PlayGames.getAchievementsClient(this@MainActivity).unlock(getString(R.string.achievement10_id))
-                    if(totalSeconds>=goals[11]) PlayGames.getAchievementsClient(this@MainActivity).unlock(getString(R.string.achievement11_id))
-                    if(totalSeconds>=goals[12]) PlayGames.getAchievementsClient(this@MainActivity).unlock(getString(R.string.achievement12_id))
-                    if(totalSeconds>=goals[13]) PlayGames.getAchievementsClient(this@MainActivity).unlock(getString(R.string.achievement13_id))
-                    if(totalSeconds>=goals[14]) PlayGames.getAchievementsClient(this@MainActivity).unlock(getString(R.string.achievement14_id))
+                    for(i in 1..14){
+                        if(totalSeconds>=goals[i]){
+                            PlayGames.getAchievementsClient(this@MainActivity).unlock(getString(achievementsIDs[i]))
+                        }
+                    }
                 }
                 mainHandler.postDelayed(this, 1000)
             }
         })
     }
 }
-
